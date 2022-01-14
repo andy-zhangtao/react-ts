ff68dd38da50d5976365d63a0c1c457005ed2219
root <root@localhost.localdomain>
2022-01-14 14:45:54 +0800

    upgrade react and material-ui version

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { renderRoutes } from 'react-router-config';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import { Provider as StoreProvider } from 'react-redux';
-import { ThemeProvider } from '@material-ui/styles';
-import MomentUtils from '@date-io/moment';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import store from './redux/store';
-import routes from './routes';
-import theme from './theme';
-
-const history = createBrowserHistory();
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <StoreProvider store={store}>
-        <ThemeProvider theme={theme}>
-            <MuiPickersUtilsProvider utils={MomentUtils}>
-                <Router history={history}>
-                    {renderRoutes(routes)}
-                </Router>
-            </MuiPickersUtilsProvider>
-        </ThemeProvider>
-    </StoreProvider>,
-    document.querySelectorAll('.app')[0]
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
